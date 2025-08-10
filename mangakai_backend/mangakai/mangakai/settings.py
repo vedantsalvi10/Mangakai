123b34cd4feaeb3ec2c581f7ca7394a93853d6ac
vedantsalvi10 <vedantsalvi10@gmail.com>
2025-08-10 22:01:39 +0530

    allowing hosts

--- mangakai_backend/mangakai/mangakai/settings.py
+++ mangakai_backend/mangakai/mangakai/settings.py
@@ -17,7 +17,8 @@ pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+ALLOWED_HOSTS = ["mangakai.in"]
+CSRF_TRUSTED_ORIGINS = ["https://mangakai.in/"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
